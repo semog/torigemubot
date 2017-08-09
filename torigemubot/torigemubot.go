ce29dabb12db6e2519936672a46dbf8a2a66438a
semog <semog@users.noreply.github.com>
2017-08-09 08:23:50 -0700

    Implement separate chat game tracking. Implement word history tracking. Refactor to use Go slices instead of List.

--- torigemubot/torigemubot.go
+++ torigemubot/torigemubot.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"container/list"
 	"fmt"
 	tg "github.com/go-telegram-bot-api/telegram-bot-api"
 	"log"
@@ -15,6 +14,7 @@ Use the /setcommands command and reply with the following list of commands.
 newgame - Start a new game with a fresh word list.
 showword - Show the current word.
 showscores - Show the current scores.
+showhistory - Show the words that have been used in the game.
 challenge - Challenge the word that was entered.
 help - Display game rules and other instructions.
 shutdown - Shutdown the bot (DEBUG ONLY)
@@ -28,15 +28,26 @@ var torigemubot = botEventHandlers{
 
 // TODO: Keep historical record of words played. Will be used to verify words are not reused, and for reverting after a challenge.
 // TODO: Create a map of chatID -> currentWord, chatID -> players.
-var currentWord string
+
+// WordMap will track the current word for each game.
+type WordMap map[int64]string
+
+// WordHistoryMap type to track the words used for each game.
+type WordHistoryMap map[int64][]string
+
+var currentWord WordMap
+var usedWords WordHistoryMap
+
+// PlayerDataMap tracks players in each game
+type PlayerDataMap map[int64][]*tg.User
 
 // TODO: Enhance the players struct to track their current score.
-var players *list.List
+var players PlayerDataMap
 
 func torigemubotOnMessage(bot *tg.BotAPI, msg *tg.Message) bool {
 	msgTextCmd := strings.ToLower(msg.Text)
 
-	log.Printf("[%s] %s", msg.From.UserName, msg.Text)
+	log.Printf("MsgFrom: Chat %s, User %s %s", getGameName(msg.Chat), getUserDisplayName(msg.From), msg.Text)
 	switch {
 	case strings.HasPrefix(msgTextCmd, "/newgame"):
 		doNewGame(bot, msg)
@@ -44,6 +55,8 @@ func torigemubotOnMessage(bot *tg.BotAPI, msg *tg.Message) bool {
 		doShowWord(bot, msg)
 	case strings.HasPrefix(msgTextCmd, "/showscores"):
 		doShowScores(bot, msg)
+	case strings.HasPrefix(msgTextCmd, "/showhistory"):
+		doShowHistory(bot, msg)
 	case strings.HasPrefix(msgTextCmd, "/challenge"):
 		doChallenge(bot, msg)
 	case strings.HasPrefix(msgTextCmd, "/help"):
@@ -58,7 +71,7 @@ func torigemubotOnMessage(bot *tg.BotAPI, msg *tg.Message) bool {
 }
 
 func torigemubotOnInlineQuery(bot *tg.BotAPI, query *tg.InlineQuery) bool {
-	log.Printf("OnInlineQuery: %s %s [%s] %s", query.From.FirstName, query.From.LastName, query.From.UserName, query.Query)
+	log.Printf("OnInlineQuery: %s %s", getUserDisplayName(query.From), query.Query)
 	var answer = tg.InlineConfig{
 		InlineQueryID: query.ID,
 		IsPersonal:    true}
@@ -77,32 +90,15 @@ func doNewGame(bot *tg.BotAPI, msg *tg.Message) {
 	// If someone objects, then the reset is canceled.
 	newGame(msg.Chat)
 	joinGame(bot, msg)
-	bot.Send(tg.NewMessage(msg.Chat.ID, fmt.Sprintf("New game started by %s %s.\nWho wants to go first?", msg.From.FirstName, msg.From.LastName)))
-}
-
-// For when a user leaves the chat.
-func doLeave(bot *tg.BotAPI, msg *tg.Message) {
-	log.Println("Received leave command.")
-	pelem := findPlayer(msg.Chat, msg.From)
-	if pelem == nil {
-		return
-	}
-
-	players.Remove(pelem)
-	player := pelem.Value.(*tg.User)
-	bot.Send(tg.NewMessage(msg.Chat.ID, fmt.Sprintf("%s %s left the game.", player.FirstName, player.LastName)))
-	if players.Len() == 0 {
-		// Game over.
-		newGame(msg.Chat)
-	}
+	bot.Send(tg.NewMessage(msg.Chat.ID, fmt.Sprintf("New game started by %s.\nWho wants to go first?", getUserDisplayName(msg.From))))
 }
 
 func doShowWord(bot *tg.BotAPI, msg *tg.Message) {
 	var reply string
-	if len(currentWord) == 0 {
+	if currentWord == nil || len(currentWord[msg.Chat.ID]) == 0 {
 		reply = "There is no current word."
 	} else {
-		reply = fmt.Sprintf("Current word: %s", currentWord)
+		reply = fmt.Sprintf("Current word: %s", currentWord[msg.Chat.ID])
 	}
 	bot.Send(tg.NewMessage(msg.Chat.ID, reply))
 }
@@ -110,6 +106,11 @@ func doShowWord(bot *tg.BotAPI, msg *tg.Message) {
 func doShowScores(bot *tg.BotAPI, msg *tg.Message) {
 	log.Println("Received showscores command.")
 	bot.Send(tg.NewMessage(msg.Chat.ID, "Four Score and Seven Words Ago..."))
+}
+
+func doShowHistory(bot *tg.BotAPI, msg *tg.Message) {
+	log.Println("Received showhistory command.")
+	bot.Send(tg.NewMessage(msg.Chat.ID, "Words used so far:\n"+strings.Join(usedWords[msg.Chat.ID], "\n")))
 }
 
 func doChallenge(bot *tg.BotAPI, msg *tg.Message) {
@@ -121,7 +122,16 @@ func doWordEntry(bot *tg.BotAPI, msg *tg.Message) {
 	log.Println("Received a NEW word!.")
 	// Auto-join the game.
 	joinGame(bot, msg)
-	currentWord = msg.Text
+	if currentWord == nil {
+		currentWord = make(WordMap)
+	}
+	currentWord[msg.Chat.ID] = msg.Text
+
+	if usedWords == nil {
+		usedWords = make(WordHistoryMap)
+	}
+	usedWords[msg.Chat.ID] = append(usedWords[msg.Chat.ID], msg.Text)
+
 	// TODO: Calculate scores. If only one person, then no scores awarded.
 	// TODO: Display the name of person and amount of points won/lost for this word entry.
 	doShowWord(bot, msg)
@@ -139,44 +149,70 @@ func doShutdown(bot *tg.BotAPI, msg *tg.Message) {
 
 // TODO: Do a new game if the bot is kicked out of chat.
 func newGame(chat *tg.Chat) {
-	currentWord = ""
-	players = nil
+	if currentWord != nil {
+		delete(currentWord, chat.ID)
+	}
+	if usedWords != nil {
+		delete(usedWords, chat.ID)
+	}
+	if players != nil {
+		delete(players, chat.ID)
+	}
 }
 
 func joinGame(bot *tg.BotAPI, msg *tg.Message) {
 	if players == nil {
-		players = list.New()
+		players = make(PlayerDataMap)
 	}
 
 	player := msg.From
-	if findPlayer(msg.Chat, player) == nil {
-		var gamename string
-
-		switch msg.Chat.Type {
-		case "group":
-			gamename = fmt.Sprintf("%s [%d]", msg.Chat.Title, msg.Chat.ID)
-		default:
-			gamename = fmt.Sprintf("%s %s [%d]", msg.Chat.FirstName, msg.Chat.LastName, msg.Chat.ID)
-		}
-		log.Printf("Adding player %s %s [%s] to game %s.", player.FirstName, player.LastName, player.UserName, gamename)
-		players.PushBack(player)
-		bot.Send(tg.NewMessage(msg.Chat.ID, fmt.Sprintf("%s %s joined the game.", player.FirstName, player.LastName)))
+	if _, index := findPlayer(msg.Chat.ID, player); index < 0 {
+		log.Printf("Adding player %s to game %s.", getUserDisplayName(player), getGameName(msg.Chat))
+		players[msg.Chat.ID] = append(players[msg.Chat.ID], player)
+		bot.Send(tg.NewMessage(msg.Chat.ID, fmt.Sprintf("%s joined the game.", getUserDisplayName(player))))
 	}
 }
 
-func findPlayer(chat *tg.Chat, user *tg.User) *list.Element {
-	if players == nil {
-		return nil
+func getGameName(chat *tg.Chat) string {
+	switch chat.Type {
+	case "group":
+		return fmt.Sprintf("%s [%d]", chat.Title, chat.ID)
+	default:
+		return fmt.Sprintf("%s %s [%d]", chat.FirstName, chat.LastName, chat.ID)
+	}
+}
+
+func getUserDisplayName(user *tg.User) string {
+	return fmt.Sprintf("%s %s (%s)", user.FirstName, user.LastName, user.UserName)
+}
+
+// For when a user leaves the chat.
+func leaveGame(bot *tg.BotAPI, msg *tg.Message) {
+	player, index := findPlayer(msg.Chat.ID, msg.From)
+	if player == nil {
+		return
 	}
 
-	var player *tg.User
-	for e := players.Front(); e != nil; e = e.Next() {
-		player = e.Value.(*tg.User)
+	currentPlayers := players[msg.Chat.ID]
+	players[msg.Chat.ID] = append(currentPlayers[:index], currentPlayers[index+1:]...)
+	bot.Send(tg.NewMessage(msg.Chat.ID, fmt.Sprintf("%s left the game.", getUserDisplayName(player))))
+	if len(players[msg.Chat.ID]) < 2 {
+		// Game over.
+		newGame(msg.Chat)
+	}
+}
+
+func findPlayer(chatID int64, user *tg.User) (*tg.User, int) {
+	if players == nil {
+		return nil, -1
+	}
+
+	for i, player := range players[chatID] {
 		if player.FirstName == user.FirstName &&
 			player.LastName == user.LastName &&
 			player.UserName == user.UserName {
-			return e
+			return player, i
 		}
 	}
-	return nil
+	return nil, -1
 }
