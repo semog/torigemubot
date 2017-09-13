52a219283b79e27498a1a42ed3c18f9fff5ae088
semog <semog@users.noreply.github.com>
2017-09-12 21:39:44 -0700

    Store kanji points lookup in a database table for adding custom words at runtime. Update the scoring calculation to handle kanji that didn't have a grade or JLPT number.

--- mkkanjidb/mkkanjidb.go
+++ mkkanjidb/mkkanjidb.go
@@ -18,9 +18,9 @@ const dbFilename = "torigemu.db"
 
 const jmdictFileName = "jmdict.xml"
 const kanjidictFileName = "kanjidic2.xml"
-const maxPts = 15  // Max grade of 10 + Max JLPT of 5
-const maxLimit = 4 // Arbitrary limit
-const ptsFactor = maxPts / maxLimit
+const maxPts = 15.0   // Max grade of 10 + Max JLPT of 5
+const maxLimit = 4.0  // Arbitrary limit
+const ptsFactor = 3.0 // maxPts / maxLimit
 const maxJLPT = 6
 
 type kmap map[string]int
@@ -61,10 +61,43 @@ func createKanjiDb(dict *jmdict, kptsmap kmap) error {
 		return err
 	}
 	dropTable("words")
+	dropTable("kanjipoints")
 	err = createTable("words (seq TEXT, kanji TEXT PRIMARY KEY, kana TEXT, points INT)")
 	if err != nil {
 		return err
 	}
+	err = createTable("kanjipoints (kanji TEXT PRIMARY KEY, points INT)")
+	if err != nil {
+		return err
+	}
+	insertKanjiPoints(kptsmap)
+	insertWords(dict, kptsmap)
+	return nil
+}
+
+func insertKanjiPoints(kptsmap kmap) error {
+	var err error
+	// Prepare the statement and use a transaction for massive speed increase.
+	insertStmt, err = db.Prepare("INSERT INTO kanjipoints (kanji, points) VALUES (:KJ, :SC)")
+	if err != nil {
+		return err
+	}
+	defer insertStmt.Close()
+	// Optimize the database insertion
+	execDb("BEGIN")
+	for k, p := range kptsmap {
+		_, err = insertStmt.Exec(sql.Named("KJ", &k), sql.Named("SC", &p))
+		if err != nil {
+			execDb("ROLLBACK")
+			return err
+		}
+	}
+	execDb("COMMIT")
+	return nil
+}
+
+func insertWords(dict *jmdict, kptsmap kmap) error {
+	var err error
 	// Prepare the statement and use a transaction for massive speed increase.
 	insertStmt, err = db.Prepare("INSERT INTO words (seq, kanji, kana, points) VALUES (:SQ, :KJ, :KN, :SC)")
 	if err != nil {
@@ -75,7 +108,12 @@ func createKanjiDb(dict *jmdict, kptsmap kmap) error {
 	execDb("BEGIN")
 	for _, e := range dict.Entry {
 		if isNoun(e) {
-			saveWord(e, kptsmap)
+			err = saveWord(e, kptsmap)
+			if err != nil {
+				log.Printf("Error saving seq: %d", e.Seq)
+				execDb("ROLLBACK")
+				return err
+			}
 		}
 	}
 	execDb("COMMIT")
@@ -101,7 +139,8 @@ func isNoun(e entry) bool {
 	return false
 }
 
-func saveWord(e entry, kptsmap kmap) {
+func saveWord(e entry, kptsmap kmap) error {
+	var err error
 	kana, endsInN := getKana(e)
 	kanjis := getKanjis(e)
 	seq := fmt.Sprintf("%d", e.Seq)
@@ -109,22 +148,32 @@ func saveWord(e entry, kptsmap kmap) {
 		// Get all of the entry kanji variants
 		hiragana := convertToHiragana(kana)
 		for _, kanji := range kanjis {
-			saveKanji(seq, kanji, hiragana, endsInN, kptsmap)
+			err = saveKanji(seq, kanji, hiragana, endsInN, kptsmap)
+			if err != nil {
+				return err
+			}
 		}
 	} else {
 		// Only kana for this entry.
 		for _, kn := range strings.Split(kana, ",") {
 			hiragana := convertToHiragana(kn)
-			saveKanji(seq, kn, hiragana, endsInN, kptsmap)
+			err = saveKanji(seq, kn, hiragana, endsInN, kptsmap)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	// Handle nokanji variations for this entry.
 	nokanjis := getNoKanjis(e)
 	if len(nokanjis) > 0 {
 		for _, nkn := range nokanjis {
-			saveKanji(seq, nkn, convertToHiragana(nkn), endsInNExp.MatchString(nkn), kptsmap)
+			err = saveKanji(seq, nkn, convertToHiragana(nkn), endsInNExp.MatchString(nkn), kptsmap)
+			if err != nil {
+				return err
+			}
 		}
 	}
+	return nil
 }
 
 func convertToHiragana(kana string) string {
@@ -149,7 +198,7 @@ func convertToHiragana(kana string) string {
 	return hiragana
 }
 
-func saveKanji(seq string, kanji string, kana string, endsInN bool, kptsmap kmap) {
+func saveKanji(seq string, kanji string, kana string, endsInN bool, kptsmap kmap) error {
 	var pts int
 	if endsInN {
 		// Automatic zero for ending in 'ん'
@@ -159,22 +208,22 @@ func saveKanji(seq string, kanji string, kana string, endsInN bool, kptsmap kmap
 	}
 	_, err := insertStmt.Exec(sql.Named("SQ", seq), sql.Named("KJ", &kanji), sql.Named("KN", &kana), sql.Named("SC", &pts))
 	if err != nil {
-		mergeRecords(seq, kanji, kana, pts)
+		err = mergeRecords(seq, kanji, kana, pts)
 		failcount++
 		// return err
 	} else {
 		insertcount++
 	}
+	return err
 }
 
-func mergeRecords(seq string, kanji string, kana string, pts int) {
+func mergeRecords(seq string, kanji string, kana string, pts int) error {
 	var existingKana, existingSeq string
 	var existingPts int
 	found := queryDb(fmt.Sprintf("SELECT seq, kana, points FROM words WHERE kanji = '%s'", kanji),
 		&existingSeq, &existingKana, &existingPts)
 	if !found {
-		log.Printf("DBERROR: Could not find record for %s", kanji)
-		return
+		return fmt.Errorf("DBERROR: Could not find record for %s", kanji)
 	}
 	// Merge kana and the word pts.
 	newSeq := mergeStrings(existingSeq, seq)
@@ -183,7 +232,9 @@ func mergeRecords(seq string, kanji string, kana string, pts int) {
 	err := execDb("UPDATE words SET seq = ?, kana = ?, points = ? WHERE kanji = ?", &newSeq, &newKana, &newPts, &kanji)
 	if err != nil {
 		log.Printf("DBERROR updating %s", kanji)
+		return err
 	}
+	return nil
 }
 
 func mergeStrings(first string, second string) string {
@@ -321,13 +372,18 @@ func getKanjiPtsMap() (kmap, error) {
 }
 
 func getCharacterPts(ch character) int {
-	pts := ch.Misc.Grade
-	if ch.Misc.JLPT > 0 {
-		// JLPT is in reverse order. Higher level is lower number.
-		pts += maxJLPT - ch.Misc.JLPT
+	pts := float32(ch.Misc.Grade)
+	if pts < 1.0 {
+		pts = 1.0
 	}
+	jlpt := ch.Misc.JLPT
+	if jlpt < 1 {
+		jlpt = 5
+	}
+	// JLPT is in reverse order. Higher level is lower number.
+	pts += maxJLPT - float32(jlpt)
 	// Kanji Pts: RawPts / PtsFactor
-	return pts / ptsFactor
+	return int((pts / ptsFactor) + 0.5)
 }
 
 func loadXMLFile(filename string) ([]byte, error) {
